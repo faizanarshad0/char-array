371deeaf11dcfefcc0cd66619d90b95081de7064
faizanarshad0 <123149939+faizanarshad0@users.noreply.github.com>
2023-01-20 20:41:04 +0500

    Add files via upload

---
+++ char arr.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+//#include<string.h>
+using namespace std;
+class stack
+{
+	private:
+		char arr[i];
+		string_arr[i]={(a+b)+(c+d)}
+	char 'c';
+	public:
+	void	push(char 'c' )
+		{
+			if(top=size-1)
+			{
+				cout<<"balanced"<<endl;
+			}
+			else
+			cout<<"unbalanced"<<endl;
+			char arr[i];
+        	string_arr[i]={(a+b)+(c+d)}
+        	for(int i<=n;i++)
+			return 0;
+		}
+	void	pop()
+		{
+			
+		}
+};
+int main()
+{
+/*	int n;
+	char arr[i];
+	string_arr[i]={(a+b)+(c+d)}
+	for(int i<=n;i++)
+	if(arr[i]=='(')
+	{
+		
+	}*/
+	
+	//stack
+}
+
